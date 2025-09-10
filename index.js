1bf00364b74ce73cef27c79efff616f8a56fcda4
abouzaid142003 <abouzaid.142003@gmail.com>
2025-09-10 20:05:10 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Replit + GitHub!");
+const { exec } = require("child_process");
+
+// تشغيل n8n
+const process = exec("n8n");
+
+process.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+process.stderr.on("data", (data) => {
+  console.error(data.toString());
+});
